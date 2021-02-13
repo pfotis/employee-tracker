e629d95a593e562eb853b298b030de097a6c9f68
pfotis <69961508+pfotis@users.noreply.github.com>
2021-02-13 21:17:32 +1100

    create the file seed.sql

---
+++ assets/templates/seed.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employeeDB;
+
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE department (
+	id INTEGER(10),
+    name VARCHAR(30),
+    primary key(id)
+);
+
+CREATE TABLE role (
+	id INTEGER(10),
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER(10),
+    primary key(id)
+);
+
+CREATE TABLE role (
+	id INTEGER(10),
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER(10),
+    manager_id INTEGER(10),
+    primary key(id)
+);
