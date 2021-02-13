2e566bc4d676b58f2754227d52dbc95f1b5a48d5
pfotis <69961508+pfotis@users.noreply.github.com>
2021-02-14 00:27:59 +1100

    create file connection.js in the forder config

---
+++ assets/config/connection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'p@ssworD',
+        database: 'employeeDB',
+    });
+}
+
+module.exports = connection;
