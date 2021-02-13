fc95e9d78450c375a0bfd74de4c0b673c4f7cdaa
pfotis <69961508+pfotis@users.noreply.github.com>
2021-02-13 20:53:05 +1100

    all the npm install require and create the file employeeTracker.js

---
+++ employeeTracker.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Be sure to update with your own MySQL password!
+  password: '',
+  database: '',
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  runQuestions();
+});
+
+const runQuestions = () =>{
+
+};
